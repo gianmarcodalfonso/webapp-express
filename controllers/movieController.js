59f8c270bf4dbcff82f7c1270846a35acd26823d
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-09-18 19:34:02 +0200

    feat: index-connection

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -3,7 +3,16 @@ const connection = require("../data/db");
 
 //index
 const index = (req, res) => {
-	console.log("index");
+	const sql = "SELECT * FROM movies";
+
+	connection.query(sql, (err, results) => {
+		if (err)
+			return res
+				.status(500)
+				.json({ error: `Errore nell'esecuzione della query: ${err}` });
+
+		res.send(results);
+	});
 };
 
 //show
--- data/db.js
+++ data/db.js
@@ -17,3 +17,5 @@ connection.connect((err) => {
 		console.log(`Connessione al db avvenuta correttamente`);
 	}
 });
+
+module.exports = connection;
